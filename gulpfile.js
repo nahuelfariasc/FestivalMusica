d3373401a021fc01dfa95408c22e2ab89a445e03
nahuelfc <nahuelfariasc@gmail.com>
2022-06-30 15:54:36 -0300

    nahuel

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
 
@@ -35,7 +36,16 @@ function versionWebp(done) {
     src('scr/img/**/*.{png,jpg}')
         .pipe(webp(opciones))
         .pipe(dest('build/img'));
+    done();
+}
 
+function versionAvif(done) {
+    const opciones = {
+        quality: 50
+    };
+    src('scr/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
     done();
 }
 
@@ -48,4 +58,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
