6bff6f3c25ef4c438bc5acdc1c5d912b868c6fc6
nahuelfc <nahuelfariasc@gmail.com>
2022-07-15 16:02:46 -0300

    nahuel

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const {src, dest, watch, parallel} = require("gulp");
 // CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -10,11 +14,17 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+// JS
+const terser = require('gulp-terser-js');
+
 function css(done) {
 
     src('scr/scss/**/*.scss')    // identifica el archivo de SASS
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass())     // compilarlo
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'));    //  almancenarlo en el disco duro
     done(); // callback q avisa a gulp cuando llegamos al final
 }
@@ -51,6 +61,9 @@ function versionAvif(done) {
 
 function javascript(done) {
     src('scr/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
     done();
 }
